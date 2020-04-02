f05feceee7fe8b059f0c5ec782b6aaee413b9554
Christian Meyer <christian.meyer@gmail.com>
2020-04-02 13:09:50 +0200

    use more spring test features

--- src/test/kotlin/com/bol/mancala/controller/GameControllerTest.kt
+++ src/test/kotlin/com/bol/mancala/controller/GameControllerTest.kt
@@ -2,19 +2,17 @@ package com.bol.mancala.controller
 
 import com.bol.mancala.BeansInitializer
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
-import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestConstructor
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.post
 
-@ContextConfiguration(initializers = [BeansInitializer::class])
 @WebMvcTest(controllers = [GameController::class])
-class GameControllerTest {
-
-    @Autowired
-    private lateinit var mockMvc: MockMvc
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@SpringJUnitWebConfig(initializers = [BeansInitializer::class])
+class GameControllerTest(private val mockMvc: MockMvc) {
 
     @Test
     fun `create a new game`() {
